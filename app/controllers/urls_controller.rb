40e901647534e215ea67c20bf2912108a6a2481d
itr1214 <oichiro@linkstart.jp>
2022-06-12 22:28:53 +0900

    修正

--- app/controllers/urls_controller.rb
+++ app/controllers/urls_controller.rb
@@ -1,19 +1,30 @@
 class UrlsController < ApplicationController
-
+  before_action :returnHost
   def index
+    short_urls = Shortener::ShortenedUrl.pluck(:url,:unique_key)
+    urls = []
+    short_urls.each_with_index do |value, index|
+      urls[index] = [value[0], "#{@url}/#{value[1]}"]
+    end
     # binding.pry
-    urls = Shortener::ShortenedUrl.pluck(:url,:unique_key)
     render json: {status: 200, urls: urls}
   end
 
   def create
     short_url = Shortener::ShortenedUrl.generate(url_params['original_url'])
-    render json: {status: 200, short_url: short_url.unique_key}
+    render json: {status: 200, short_url: "#{@url}/#{short_url.unique_key}"}
   end
 
   def to_original_url
     short_url = Shortener::ShortenedUrl.find_by(unique_key: params[:id])
     render json: {status: 200, original_url: short_url.url}
+  end
+
+  def returnHost
+    @host = request.host
+    @port = request.port
+    protocol = request.protocol
+    @url = "#{protocol}#{@host}:#{@port}"
   end
 
   private
